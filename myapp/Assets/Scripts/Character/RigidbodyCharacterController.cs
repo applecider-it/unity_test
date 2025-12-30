a42ce15e1d0d2341f77343cf993826a5a86a810f
owner <owner@localhost>
2025-12-30 15:58:48 +0900

    移動処理をまとめた

--- myapp/Assets/Scripts/Character/RigidbodyCharacterController.cs
+++ myapp/Assets/Scripts/Character/RigidbodyCharacterController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class RigidbodyCharacterController : MonoBehaviour
 {
     [Header("Main")]
+
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveSpeedAir = 0.05f;
     [SerializeField] float jumpForce = 5f;
     [SerializeField] float groundCheckDistance = 0.1f;
     [SerializeField] float rotationSpeed = 10f;
@@ -16,8 +18,9 @@ public class RigidbodyCharacterController : MonoBehaviour
     // private
 
     Vector2 moveInput;  // 移動方向
-
     bool jump;  // ジャンプフラグ
+
+    int jumpCnt = 0;
 
     Rigidbody rb;
     bool isGrounded;
@@ -35,79 +38,159 @@ public class RigidbodyCharacterController : MonoBehaviour
         Jump();
     }
 
+    /// <summary>
+    /// 移動処理
+    /// </summary>
     void Move()
     {
-        // 入力なし → 即停止
+        // 重力の影響を与えたあとの、Velocity.y
+        float nextVY = rb.linearVelocity.y - gravity;
+
         if (moveInput.sqrMagnitude < 0.01f)
         {
-            rb.linearVelocity = new Vector3(0f, (isGrounded ? 0f : rb.linearVelocity.y - gravity), 0f);
-            return;
+            // 入力なしの時
+
+            StopProccesInMove(nextVY);
         }
+        else
+        {
+            // 入力ありの時
 
-        // 入力あり
-        Vector3 moveDir = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+            Vector3 moveDir = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
 
-        // ===== 移動処理 =====
+            // ===== 移動処理 =====
 
-        Vector3 slopeMoveDir = Vector3.ProjectOnPlane(moveDir, groundNormal).normalized;
+            MoveProccesInMove(nextVY, moveDir);
 
-        Vector3 targetVelocity = slopeMoveDir * moveSpeed;
+            // ===== 回転処理 =====
 
-        rb.linearVelocity = new Vector3(
-            targetVelocity.x,
-            (isGrounded ? targetVelocity.y : rb.linearVelocity.y - gravity),
-            targetVelocity.z
-        );
-
-        // ===== 回転処理 =====
-
-        // 移動方向を向く回転
-        Quaternion targetRotation = Quaternion.LookRotation(moveDir);
-
-        // 現在の向きからスムーズに補間
-        rb.MoveRotation(
-            Quaternion.Slerp(
-                rb.rotation,
-                targetRotation,
-                rotationSpeed * Time.fixedDeltaTime
-            )
-        );
+            RotationProccesInMove(moveDir);
+        }
     }
 
+    void MoveProccesInMove(float nextVY, Vector3 moveDir)
+    {
+        if (isGrounded)
+        {
+            // 地面にいるとき
+
+            Vector3 slopeMoveDir = Vector3.ProjectOnPlane(moveDir, groundNormal).normalized;
+
+            Vector3 targetVelocity = slopeMoveDir * moveSpeed;
+
+            rb.linearVelocity = new Vector3(
+                targetVelocity.x,
+                targetVelocity.y,
+                targetVelocity.z
+            );
+        }
+        else
+        {
+            // 地面にいないとき
+
+            rb.linearVelocity = new Vector3(
+                rb.linearVelocity.x,
+                nextVY,
+                rb.linearVelocity.z
+            ) + (moveDir * moveSpeedAir);
+        }
+    }
+
+    void StopProccesInMove(float nextVY)
+    {
+        if (isGrounded)
+        {
+            // Velocity.yを0にすることで、上り坂で止まった時に跳ねないようになる
+            rb.linearVelocity = new Vector3(0f, 0f, 0f);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector3(
+                rb.linearVelocity.x,
+                nextVY,
+                rb.linearVelocity.z
+            );
+        }
+    }
+
+    void RotationProccesInMove(Vector3 moveDir)
+    {
+        if (isGrounded)
+        {
+            // 移動方向を向く回転
+            Quaternion targetRotation = Quaternion.LookRotation(moveDir);
+
+            // 現在の向きからスムーズに補間
+            rb.MoveRotation(
+                Quaternion.Slerp(
+                    rb.rotation,
+                    targetRotation,
+                    rotationSpeed * Time.fixedDeltaTime
+                )
+            );
+        }
+    }
+
+    /// <summary>
+    /// ジャンプ処理
+    /// </summary>
     void Jump()
     {
+        if (jumpCnt > 0) jumpCnt--;
+
         if (jump)
         {
             if (isGrounded)
             {
+                // 地面にいるとき
+
                 rb.linearVelocity = new Vector3(
                     rb.linearVelocity.x,
                     jumpForce,
                     rb.linearVelocity.z
                 );
+
+                jumpCnt = 5;
+
             }
+
             jump = false;
         }
     }
 
+    /// <summary>
+    /// 地面判定
+    /// </summary>
     void CheckGround()
     {
-        float space = 0.1f;
+        // ジャンプ直後は、判定をスキップ
+        if (jumpCnt > 0)
+        {
+            isGrounded = false;
+            groundNormal = Vector3.up;
+
+            return;
+        }
+
+        // 足元にレイを飛ばして検査
+        float space = 0.5f;
+        float radius = 0.3f; // キャラの足の太さに合わせる
+
         RaycastHit hit;
-        isGrounded = Physics.Raycast(
-            transform.position + (Vector3.up * space),
-            Vector3.down,
+        isGrounded = Physics.SphereCast(
+            transform.position + Vector3.up * space, // 開始位置
+            radius,                                  // 球の半径
+            Vector3.down,                            // 下方向
             out hit,
-            groundCheckDistance + space + 0.01f,
+            groundCheckDistance + space - radius,             // 距離
             groundLayer
         );
 
         groundNormal = isGrounded ? hit.normal : Vector3.up;
-
-        //Debug.Log($"isGrounded {isGrounded}");
     }
 
     // setter
+
     public void SetMoveInput(Vector2 value)
     {
         moveInput = value;
